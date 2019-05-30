ce0960b3e881f42481074f7937bf37c2f6f4f4c7
Fabrício Zimmerer Murta <fabricio@object.net>
2019-05-30 17:39:16 -0300

    Adds descriptive comments to issue 3977 unit tests.

--- Tests/Batch3/BridgeIssues/3900/N3977.cs
+++ Tests/Batch3/BridgeIssues/3900/N3977.cs
@@ -1,11 +1,18 @@
 using Bridge.Test.NUnit;
-using System;
+using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// Ensures Linq's .Join() extension works for referencing inner and outer
+    /// parameter tables.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3977 - {0}")]
     public class Bridge3977
     {
+        /// <summary>
+        /// A simple struct to base the tests with.
+        /// </summary>
         struct stringHolder
         {
             public readonly string _val;
@@ -19,6 +26,11 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             }
         }
 
+        /// <summary>
+        /// Tests by joining two arrays of stringHolder structs, by inspecting
+        /// the individual values. If any happens to be 'null', then the
+        /// feature is broken.
+        /// </summary>
         [Test]
         public static void TestDirectJoinInvocation()
         {
@@ -27,31 +39,33 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
                 "goodbye"
             };
 
-                var table2 = new stringHolder[] {
+            var table2 = new stringHolder[] {
                 "world",
                 "everybody"
             };
 
-            System.Linq.Enumerable.Join(
-                    inner: table1,
-                    outer: table2,
-                    innerKeySelector: row1 => true,
-                    outerKeySelector: row2 => true,
-                    resultSelector: (stringHolder t1Val, stringHolder t2Val) => {
-                        if (t2Val._val == null)
-                        {
-                            Assert.Fail("t2Val._val == null");
-                        }
-
-                        if (t1Val._val == null)
-                        {
-                            Assert.Fail("t1Val._val == null");
-                        }
-
-                        Assert.True(t1Val._val == "world" || t1Val._val == "everybody");
-                        Assert.True(t2Val._val == "hello" || t2Val._val == "goodbye");
-                        return true;
+            Enumerable.Join(
+                inner: table1,
+                outer: table2,
+                innerKeySelector: row1 => true,
+                outerKeySelector: row2 => true,
+                resultSelector: (stringHolder t1Val, stringHolder t2Val) =>
+                {
+                    if (t2Val._val == null)
+                    {
+                        Assert.Fail("t2Val._val == null");
                     }
+
+                    if (t1Val._val == null)
+                    {
+                        Assert.Fail("t1Val._val == null");
+                    }
+
+                    Assert.True(t1Val._val == "world" || t1Val._val == "everybody", "table 1 element value is '" + t1Val._val + "'.");
+                    Assert.True(t2Val._val == "hello" || t2Val._val == "goodbye", "table 2 element value is '" + t2Val._val + "'.");
+
+                    return true;
+                }
              ).All(retc => retc == true);
         }
     }
