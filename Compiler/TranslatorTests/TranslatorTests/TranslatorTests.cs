3344c22c6f9992ba8ccdf1f814f8f59d6e7d1a9c
ProductiveRage <dangger36@gmail.com>
2019-06-06 10:07:58 +0200

    Add a test for PR #3990 - can ReadStream now read streams that are larger than 1MB and not precisely divisible by 1MB?

---
+++ Compiler/TranslatorTests/TranslatorTests/TranslatorTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System.Linq;
+
+namespace Bridge.Translator.Tests
+{
+    [TestFixture]
+    internal class TranslatorTests
+    {
+        [Test]
+        public void Issue3989()
+        {
+            var data = Enumerable.Range(0, (int)(1.5 * 1024 * 1024)).Select(i => (byte)(i % 255)).ToArray();
+            byte[] dataFromStream;
+            using (var stream = new MemoryStream(data))
+            {
+                dataFromStream = new TestTranslator().TestReadStream(stream);
+            }
+            Assert.AreEqual(data, dataFromStream);
+        }
+
+        private class TestTranslator : Translator
+        {
+            public TestTranslator() : base("test") { }
+
+            public byte[] TestReadStream(Stream stream) => base.ReadStream(stream);
+        }
+    }
+}
