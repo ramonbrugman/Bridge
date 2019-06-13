2ddef0eeadbb1192942f7dcf0f7a9e0cf41dee5a
Fabrício Zimmerer Murta <fabricio@object.net>
2019-06-13 16:10:11 -0300

    Reviews unit tests for issue 3994.

--- Tests/Batch3/BridgeIssues/3900/N3994.cs
+++ Tests/Batch3/BridgeIssues/3900/N3994.cs
@@ -1,26 +1,41 @@
 using Bridge.Test.NUnit;
-using System;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// Ensures default value in method parameters for enum works.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3994 - {0}")]
     public class Bridge3994
     {
+        /// <summary>
+        /// An enum to use as default parameter to the test method.
+        /// </summary>
         public enum TestEnum
         {
             Test,
             Test01
         }
 
+        /// <summary>
+        /// A test method taking as parameter the enum above, and choosing
+        /// its first value as the default.
+        /// </summary>
+        /// <param name="testEnum">Enum parameter taking a default.</param>
+        /// <returns>THe specified (or default) enum.</returns>
         private static TestEnum TestFnc(TestEnum testEnum = TestEnum.Test)
         {
             return testEnum;
         }
 
+        /// <summary>
+        /// Tests by calling the method without providing the value, so the
+        /// return is the default enum value.
+        /// </summary>
         [Test]
         public static void TestEnumOptionalParam()
         {
-            Assert.AreEqual(TestEnum.Test, TestFnc());
+            Assert.AreEqual(TestEnum.Test, TestFnc(), "Method with enum parameter's default value works.");
         }
     }
 }
