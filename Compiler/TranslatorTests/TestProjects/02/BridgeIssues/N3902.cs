4a484da84fbd283d982fe9bef28eb1564957564e
Fabrício Zimmerer Murta <fabricio@object.net>
2019-06-12 23:35:28 -0300

    Adds NUnit Integration tests for issue 3902 and 3986.
    Added non-minified (3902) and minified (3986) test cases for projects in
    their respective integration tests' projects, as well as updates the
    reference files.
    
    This will ensure, for 3986, easy to find issues if the minified output
    ever results in non-escaped sequences, at least to the sample provided
    in issue 3902's report.

---
+++ Compiler/TranslatorTests/TestProjects/02/BridgeIssues/N3902.cs
@@ -0,0 +1,14 @@
+﻿using Bridge;
+using System;
+
+namespace Test.BridgeIssues.N3902
+{
+    public class N3902
+    {
+        public void UnicodeStringTranslateFormatted()
+        {
+            string unicode_escaped_string = "\ud8a1\ud8a2\ue681";
+            Console.WriteLine(unicode_escaped_string);
+        }
+    }
+}
---
+++ Compiler/TranslatorTests/TestProjects/03/BridgeIssues/N3986.cs
@@ -0,0 +1,14 @@
+﻿using Bridge;
+using System;
+
+namespace Test.BridgeIssues.N3986
+{
+    public class N3986
+    {
+        public void UnicodeStringTranslateMinified()
+        {
+            string unicode_escaped_string = "\ud8a1\ud8a2\ue681";
+            Console.WriteLine(unicode_escaped_string);
+        }
+    }
+}
