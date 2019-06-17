1a43cd2a7f83571d88e83a3b80aea9144f2d0cc3
Fabrício Zimmerer Murta <fabricio@object.net>
2019-06-17 04:23:55 -0300

    Adds descriptive comments to issue #3989 unit tests.

--- Compiler/TranslatorTests/TranslatorTests/TranslatorTests.cs
+++ Compiler/TranslatorTests/TranslatorTests/TranslatorTests.cs
@@ -4,9 +4,29 @@ using System.Linq;
 
 namespace Bridge.Translator.Tests
 {
+    /// <summary>
+    /// The test here consists in ensuring Bridge.Translator.ReadStream()
+    /// supports streams longer than 1MB in size.
+    /// </summary>
     [TestFixture]
     internal class TranslatorTests
     {
+        /// <summary>
+        /// Wraps Bridge.Translator.ReadStream() in a local TestTranslator
+        /// class.
+        /// </summary>
+        private class TestTranslator : Translator
+        {
+            public TestTranslator() : base("test") { }
+
+            public byte[] TestReadStream(Stream stream) => base.ReadStream(stream);
+        }
+
+        /// <summary>
+        /// Creates a 1.5MB stream and submits it to Translator's ReadStream
+        /// method. If the final result matches, then the stream was not
+        /// truncated.
+        /// </summary>
         [Test]
         public void Issue3989()
         {
@@ -16,14 +36,7 @@ namespace Bridge.Translator.Tests
             {
                 dataFromStream = new TestTranslator().TestReadStream(stream);
             }
-            Assert.AreEqual(data, dataFromStream);
-        }
-
-        private class TestTranslator : Translator
-        {
-            public TestTranslator() : base("test") { }
-
-            public byte[] TestReadStream(Stream stream) => base.ReadStream(stream);
+            Assert.AreEqual(data, dataFromStream, "Bridge.Translator.ReadStream() supports reading 1.5MB large streams.");
         }
     }
 }
