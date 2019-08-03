402f1dac1ff3565da3728997dfa4b295a349f0b0
Fabrício Zimmerer Murta <fabricio@object.net>
2019-08-02 22:22:10 -0300

    Adds descriptive comments to issue 4023 unit test.

--- Tests/Batch3/BridgeIssues/4000/N4023.cs
+++ Tests/Batch3/BridgeIssues/4000/N4023.cs
@@ -9,7 +9,7 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         public static void TestStringJoinTemplate()
         {
             var ids = new[] { "1", "2", "3" };
-            Assert.AreEqual("1;2;3", string.Join(";", ids ?? new string[0]));
+            Assert.AreEqual("1;2;3", string.Join(";", ids ?? new string[0]), "Null-coalescing operator works as parameter expression to string.Join().");
         }
     }
 }
