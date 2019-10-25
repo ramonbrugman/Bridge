c5a4aaad9fe5cec7be665939c1ba3cc75a083ebc
Fabrício Zimmerer Murta <fabricio@object.net>
2019-10-25 03:26:21 -0300

    Adds comments to unit tests for issues 4072 and 4076.

--- Tests/Batch3/BridgeIssues/4000/N4072.cs
+++ Tests/Batch3/BridgeIssues/4000/N4072.cs
@@ -1,12 +1,21 @@
 using Bridge.Test.NUnit;
-using System;
 using Wrapper = System.Collections.Generic.List<int>;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+
+    /// <summary>
+    /// The tests here consists in ensuring 'out Type variable' syntax is
+    /// supported on TryGet.
+    /// </summary>
     [TestFixture(TestNameFormat = "#4072 - {0}")]
     public class Bridge4072
     {
+        /// <summary>
+        /// TryGet() implementation using type 'Wrapper' as parameter.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         private static bool TryGet(out Wrapper x)
         {
             x = new Wrapper();
@@ -14,13 +23,16 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         }
 
         /// <summary>
-        /// 
+        /// Tests by simply calling the TryGet() implementation in the
+        /// required format. Notice if the x variable is declared somewhere
+        /// else and used there (as just '(out x)'), the issue related to this
+        /// test is not reproduced.
         /// </summary>
         [Test]
         public static void TestTypeAlias()
         {
             bool c = TryGet(out Wrapper x);
-            Assert.True(c);
+            Assert.True(c, "'out <type> <variable>' parameter syntax is supported.");
         }
     }
 }
